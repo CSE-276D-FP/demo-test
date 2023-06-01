c43c6f309f2789e0c6d785ab348d764b16a9871a
Calvin <cknguyen@ucsd.edu>
2023-06-01 13:09:41 -0700

    fsr + audio changes

--- Audio_Scripts/audio_using_VLC.py
+++ Audio_Scripts/audio_using_VLC.py
@@ -8,7 +8,8 @@ def play_pause(p):
         p.play()
 
 # Create a VLC media player instance
-p = vlc.MediaPlayer("C:/Users/calvi/OneDrive/Documents/CSE 276/demo-test/sound_example/sample2.mp3")
+audio_file = "C:/Users/calvi/OneDrive/Documents/CSE 276/demo-test/sound_example/sample2.mp3"
+p = vlc.MediaPlayer(audio_file)
 
 # Register the 'a' key for playing or pausing the media
 keyboard.add_hotkey('a', play_pause, args=(p,))
