05e059cb5832f19a1428baa9fca9053f4303e2e8
amquach00 <76972833+amquach00@users.noreply.github.com>
2023-06-08 15:34:27 -0700

    contAudioButton2 with no class and image script

--- Image_Scripts/display_image.py
+++ Image_Scripts/display_image.py
@@ -4,27 +4,34 @@ import pygame
 
 def display_image(image_path):
     image = pygame.image.load(image_path)
-    screen.blit(image, (0, 0))
+    image_width, image_height = image.get_size()
+    aspect_ratio = min(screen_width / image_width, screen_height / image_height)
+    scaled_width = int(image_width * aspect_ratio)
+    scaled_height = int(image_height * aspect_ratio)
+    scaled_image = pygame.transform.scale(image, (scaled_width, scaled_height))
+    x = (screen_width - scaled_width) // 2
+    y = (screen_height - scaled_height) // 2
+    screen.fill((0, 0, 0))
+    screen.blit(scaled_image, (x, y))
     pygame.display.flip()
 
 # Specify the folder path where the images are located
-folder_path = "../image_example"
+folder_path = '../image_example'
 
 # Initialize Pygame
 pygame.init()
 
-# Set the screen size based on the first image's dimensions
+# Set the screen size to 1020x600
+screen_width = 1020
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Image Viewer")
+
+# Get a list of image files in the folder
 image_files = [f for f in os.listdir(folder_path) if f.endswith((".jpg", ".jpeg", ".png"))]
 if len(image_files) == 0:
     print("No image files found in the folder.")
     sys.exit()
-
-first_image_path = os.path.join(folder_path, image_files[0])
-first_image = pygame.image.load(first_image_path)
-screen_width = first_image.get_width()
-screen_height = first_image.get_height()
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Image Viewer")
 
 # Set the initial index to 0
 current_index = 0
