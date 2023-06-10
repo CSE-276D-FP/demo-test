cf6b2e6ffedd570be8b72063b379481fad7aa4df
Calvin <cknguyen@ucsd.edu>
2023-06-10 16:09:00 -0700

    usb

--- Load_from_USB.py
+++ Load_from_USB.py
@@ -1,53 +1,37 @@
 import shutil
 import os
-import subprocess
+import glob
 
-# def is_USB_plugged_in():
-#     result = subprocess.run(['mount'], capture_output=True, text=True)
-#     output = result.stdout
+# USB source path
+usb_source = "/media/*/"
 
-#     media_devices = []
-
-#     if output:
-#         lines = output.split('\n')
-#         for line in lines:
-#             if '/media/' in line:
-#                 device = line.split(' ')[0]
-#                 media_devices.append(device)
-
-#     return media_devices
-
-# # Get the media devices
-# media_devices = is_USB_plugged_in()
-
-# if len(media_devices) > 0:
-#     # Assume the first media device found is the desired source
-#     usb_source = media_devices[0]
-#     print(f"USB source: {usb_source}")
-# else:
-#     print("No media devices found.")
-#     exit()
-
-usb_source = "/dev/sda1"
 # Destination path
-mp3_destination = "./sound_example"
+mp3_destination = "/demo-test/sound_example"
 
-all_files = os.listdir(usb_source)
+# Find the USB device mounted under /media/
+media_devices = glob.glob(usb_source)
 
-# Filter MP3 files
-mp3_files = [file for file in all_files if file.endswith('.mp3')]
+if len(media_devices) > 0:
+    usb_source = media_devices[0]
 
-# Filter image files
-image_files = [f for f in all_files if f.endswith((".jpg", ".jpeg", ".png"))]
+    all_files = os.listdir(usb_source)
 
-# Copy MP3 files to the destination folder
-for file in mp3_files:
-    source_file = os.path.join(usb_source, file)
-    destination_file = os.path.join(mp3_destination, file)
-    shutil.copy2(source_file, destination_file)
+    # Filter MP3 files
+    mp3_files = [file for file in all_files if file.endswith('.mp3')]
 
-# Copy image files to the destination folder
-for file in image_files:
-    source_file = os.path.join(usb_source, file)
-    destination_file = os.path.join(mp3_destination, file)
-    shutil.copy2(source_file, destination_file)
+    # Filter image files
+    image_files = [f for f in all_files if f.endswith((".jpg", ".jpeg", ".png"))]
+
+    # Copy MP3 files to the destination folder
+    for file in mp3_files:
+        source_file = os.path.join(usb_source, file)
+        destination_file = os.path.join(mp3_destination, file)
+        shutil.copy2(source_file, destination_file)
+
+    # Copy image files to the destination folder
+    for file in image_files:
+        source_file = os.path.join(usb_source, file)
+        destination_file = os.path.join(mp3_destination, file)
+        shutil.copy2(source_file, destination_file)
+else:
+    print("No USB device found under /media/.")
