5dd12d201a9667e852eca6430c1113e87cb04d26
amquach00 <76972833+amquach00@users.noreply.github.com>
2023-06-10 14:47:47 -0700

    s

--- Audio_Scripts/mic_recording.py
+++ Audio_Scripts/mic_recording.py
@@ -90,7 +90,7 @@ button_width = 180
 button_height = 60
 button_color = (150, 150, 150)
 button_text_color = (255, 255, 255)
-button_font = pygame.font.Font(None, 30)
+button_font = pygame.font.Font(None, 40)
 
 # Calculate the total width for both buttons
 total_button_width = button_width * 2 + 10  # Add spacing between buttons
@@ -98,7 +98,7 @@ total_button_width = button_width * 2 + 10  # Add spacing between buttons
 # Quit button
 quit_text = "Exit"
 quit_text_render = button_font.render(quit_text, True, button_text_color)
-quit_text_rect = quit_text_render.get_rect(center=(screen_width // 2 + total_button_width // 2 - button_width // 2, screen_height - button_height - 10))
+quit_text_rect = quit_text_render.get_rect(center=(button_width, button_height))
 quit_button_rect = pygame.Rect(0, 0, button_width, button_height)
 quit_button_rect.center = quit_text_rect.center
 
