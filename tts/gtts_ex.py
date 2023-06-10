f44da1ff3ef5a158c73c6a2a1fdcb02c50bba843
Hannah Hui <hannah.hui5184@gmail.com>
2023-06-10 13:04:26 -0700

    volume reduced on updated prompts

--- tts/gtts_ex.py
+++ tts/gtts_ex.py
@@ -4,23 +4,23 @@ from gtts import gTTS
 import os
 
 text_input = {
-    'pre_recording_prompt'  : '''To begin recording, press the recording button and press it again to stop.
+    'pre_recording_prompt.mp3'  : '''To begin recording, press the recording button and press it again to stop.
                                     To cancel, press any other button'''
     ,
-    'cancel_recording'      : '''Exiting recording mode.'''
+    'cancel_recording.mp3'      : '''Exiting recording mode.'''
     ,
-    'recording_started'     : '''Recording started'''
+    'recording_started.mp3'     : '''Recording started'''
     ,
-    'recording_stopped'     : '''Recording stopped'''
+    'recording_stopped.mp3'     : '''Recording stopped'''
     ,
-    'post_recording_prompt' : '''If you want to play back your recording, press the play button. 
-            If you want to save your recording, press the save button. 
-            If you do not want to save your recording, press the skip button.
-            If you want to record again, first make sure you save if desired, then press the recording button.'''
+    'post_recording_prompt.mp3' : '''To play back your recording, press the play button. 
+            To save your recording, press the save button. 
+            To discard your recording, press the skip button.
+            To record again, first make sure to save if desired, then press the recording button.'''
     ,
-    'post_recording_save'   : '''Saving, then exiting recording mode.'''
+    'post_recording_save.mp3'   : '''Saving and exiting recording mode.'''
     ,
-    'post_recording_no_save': '''Exiting recording mode without saving.'''
+    'post_recording_no_save.mp3': '''Exiting recording mode without saving.'''
 }
 
 language = 'en'
@@ -28,8 +28,12 @@ language = 'en'
 for filename in text_input:
     print("Create object")
     myobj = gTTS(text=text_input[filename], lang=language, slow=False)
-    print("Saving file" + filename + ".mp3")
-    myobj.save(filename + ".mp3")
+    print("Saving file " + filename)
+    myobj.save("temp_" + filename)
+    
+    os.system('ffmpeg -i {infile} -filter:a "volume=0.5" {outfile}'
+                .format(infile="temp_" + filename, outfile=filename))
+    os.remove("temp_" + filename)
     
 # print("Playing file")
 # os.system("mpg321 welcome.mp3")
