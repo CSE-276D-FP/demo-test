ef5943d7e824dca6c3a8435b5fa3d4a1a72b3eff
Calvin <cknguyen@ucsd.edu>
2023-06-10 16:22:38 -0700

    usb

--- Load_from_USB.py
+++ Load_from_USB.py
@@ -1,37 +1,30 @@
 import shutil
 import os
-import glob
+import subprocess
 
-# USB source path
-usb_source = "/media/*/"
-
+# Change usb source name
+usb_source = "/media/hahui/NEW VOLUME"
 # Destination path
-mp3_destination = "/demo-test/sound_example"
+mp3_destination = "./sound_example"
 
-# Find the USB device mounted under /media/
-media_devices = glob.glob(usb_source)
+all_files = os.listdir(usb_source)
 
-if len(media_devices) > 0:
-    usb_source = media_devices[0]
+# Filter MP3 files
+mp3_files = [file for file in all_files if file.endswith('.mp3')]
 
-    all_files = os.listdir(usb_source)
+# Filter image files
+image_files = [f for f in all_files if f.endswith((".jpg", ".jpeg", ".png"))]
 
-    # Filter MP3 files
-    mp3_files = [file for file in all_files if file.endswith('.mp3')]
+# Copy MP3 files to the destination folder
+for file in mp3_files:
+    source_file = os.path.join(usb_source, file)
+    destination_file = os.path.join(mp3_destination, file)
+    shutil.copy2(source_file, destination_file)
 
-    # Filter image files
-    image_files = [f for f in all_files if f.endswith((".jpg", ".jpeg", ".png"))]
+# Copy image files to the destination folder
+for file in image_files:
+    source_file = os.path.join(usb_source, file)
+    destination_file = os.path.join(mp3_destination, file)
+    shutil.copy2(source_file, destination_file)
 
-    # Copy MP3 files to the destination folder
-    for file in mp3_files:
-        source_file = os.path.join(usb_source, file)
-        destination_file = os.path.join(mp3_destination, file)
-        shutil.copy2(source_file, destination_file)
-
-    # Copy image files to the destination folder
-    for file in image_files:
-        source_file = os.path.join(usb_source, file)
-        destination_file = os.path.join(mp3_destination, file)
-        shutil.copy2(source_file, destination_file)
-else:
-    print("No USB device found under /media/.")
+exit()
