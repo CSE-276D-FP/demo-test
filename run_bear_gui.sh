3646481652db2fb659f9f4b1a6eb9c49525714fd
Calvin <cknguyen@ucsd.edu>
2023-06-12 23:58:37 -0700

    Adding comments to the main executable script for buttons and GUI

--- run_bear_gui.sh
+++ run_bear_gui.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+
+###
+# This is a shell script to run the necessary files for button control
+# and GUI for the bear. This will be made a desktop icon so that it can be launched
+# through the touchscreen without the need for a mouse and keyboard
+###
+
+#Go to Audio Scripts directory and run mainAudioControl for physical buttons 
 cd 
+cd demo-test/Audio_Scripts
+python mainAudioControl.py
+
+#Go to GUI directory and execute GUI script
+cd
 cd demo-test/GUI
 python GUI.py
 
